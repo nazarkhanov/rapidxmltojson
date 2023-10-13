7fc2cb97294878813330dc400da74d6b3ebd6f75
Alisher Nazarkhanov <alisher.nazarkhanov@aviata.me>
2023-10-13 15:32:04 +0600

    fix: correct sdist

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from Cython.Build import cythonize
 extension = Extension(
     name='rapidxmltojson',
     sources=['source/rapidxmltojson.pyx'],
-    package_data={'source': ['py.typed']},
     include_dirs=['include'],
     language='c++',
 )
